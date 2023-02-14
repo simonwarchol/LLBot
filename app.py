8c589dc75ce773e8359055589edef351febe26a1
Simon Warchol <simonwarchol@g.harvard.edu>
2023-02-14 08:31:17 -0500

    Removing EOS stats

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ print('config', config)
 player_list = {'Simon': {'id': '55444'}, 'Nick': {'id': '58714'}, 'Luke': {'id': '68635'},
                'Jake': {'id': '56189'}, 'Rob': {'id': '64659'}, 'Katter': {'id': '58619'},
                'CC': {'id': '64157'}, 'Katie': {'id': '53779'}, 'Daniella': {'id': '68727'},
-               'Cat': {'id': '71256'}, 'Yasmina': {'id': '72221'}, 'Kate': {'id': '68591'}}
+               'Cat': {'id': '71256'}, 'Yasmina': {'id': '72221'}, 'Kate': {'id': '68591'}, 'Sagar': {'id': '74612'},
+               'Scott': {'id': '74615'}, 'Evan': {'id': '74465'}
+               }
 
 recipients = {
     "name": config['CHAT_NAME'],
@@ -118,6 +120,83 @@ def get_weekly_scores():
     return best_list[0]
 
 
+def get_eos_stats():
+    global player_list
+    todays_scores = []
+    question_results = None
+    season_stats = []
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto('https://www.learnedleague.com/')
+        page.fill('#sidebar input[name="username"]', config['LL_USERNAME'])
+        page.fill('#sidebar input[name="password"]', config['LL_PW'])
+        page.click('#sidebar input[type="submit"]')
+        for player, id_dict in player_list.items():
+            player_id = id_dict['id']
+            page.goto('https://www.learnedleague.com/profiles.php?{id}'.format(id=player_id))
+            username = str(page.inner_html('.namecss'))
+            result_string = username
+            rundle_string = page.inner_html('.std-left-key a')
+            rundle_string = ' '.join(rundle_string.split()[0:2])
+            scores = page.inner_html('#Latest div table:first-of-type')
+            dfs = pd.read_html('<table>' + scores + '</table>')
+            if dfs and len(dfs) > 0:
+                df = dfs[0].dropna()
+                season_stats.append({
+                    'Player': username,
+                    'Rundle': rundle_string,
+                    'Place': df['Rank'].values[0],
+                    'Wins': df['W'].values[0],
+                    'Losses': df['L'].values[0],
+                    'Total Points': df['TMP'].values[0],
+                    'Point Differential': df['MPD'].values[0],
+                    'Correct Answers': df['TCA'].values[0],
+                    'Accuracy': df['TCA'].values[0] / (6 * 25)
+
+                })
+    season_stats = sorted(season_stats, key=lambda x: x['Player'])
+    season_stats_df = pd.DataFrame.from_dict(season_stats)
+    season_stats_df.to_csv('season_stats.csv', index=False)
+    #         scores = page.inner_html('div.fl_latest.fl_l_r.plresults')
+    #         page.eval_on_selector_all("div.fl_latest.fl_l_r.plresults a[href^='/wiki/']",
+    #                                   "elements => elements.map(element => element.href)")
+    #         match_link = page.eval_on_selector_all("div.fl_latest.fl_l_r.plresults a[href^='/match.php?id=']",
+    #                                                "elements => elements.map(element => element.href)")[-1]
+    #         dfs = pd.read_html(scores)
+    #         if dfs and len(dfs) > 0:
+    #             df = dfs[0].dropna()
+    #             score = df.iloc[-1, :]['Result.1']
+    #             record = df.iloc[-1, :]['Result']
+    #             result_string += ': ' + str(record) + ' ' + str(score) + ' ' + str(rundle_string)
+    #         todays_scores.append(result_string)
+    #         page.goto(match_link)
+    #         if question_results is None:
+    #             question_results = []
+    #             day_table = page.inner_html('table.QTable')
+    #             day_table_df = pd.read_html('<table>' + day_table + '</table>')
+    #             if day_table_df and len(day_table_df) > 0:
+    #                 day_table_df = day_table_df[0].dropna()
+    #                 for i in range(6):
+    #                     question = str(day_table_df.iloc[i, 1]).replace(u'\xa0', u' ')
+    #                     question_results.append({'question': question, 'smart': [], 'dumb': []})
+    #
+    #         results_rows = page.query_selector_all('table.std.tbltop_inner tbody tr')
+    #         user_row = None
+    #         for row in results_rows:
+    #             if username in row.inner_html():
+    #                 user_row = row
+    #                 break
+    #         classes = row.eval_on_selector_all("td", "elements => elements.map(element => element.className)")[1:7]
+    #         correct_questions = [e == 'c1' for e in classes]
+    #         for i, correct in enumerate(correct_questions):
+    #             if correct:
+    #                 question_results[i]['smart'].append(username)
+    #             else:
+    #                 question_results[i]['dumb'].append(username)
+    # return sorted(todays_scores), question_results
+
+
 # Post request with requests python library
 def post_text(text):
     global recipients
@@ -137,11 +216,11 @@ def post_text(text):
 
     print(r.read())
 
-
-# Post request with requests python library
+    # Post request with requests python library
 
 
 if __name__ == "__main__":
+    # get_eos_stats()
     dayno = datetime.datetime.today().weekday()
     if dayno >= 1 and dayno <= 5:
         scores, questions = get_scores()
@@ -157,10 +236,10 @@ if __name__ == "__main__":
                 question_text.append('Only ' + who + ' missed: ' + q['question'] + ' -- WOW!')
         for qq in question_text:
             post_text(qq)
-    if dayno == 5:
-        best = get_weekly_scores()
-        post_str = "🍰 ⋆ 🍉  🎀 Congrats to " + best[
-            'person'] + " for winning '𝐿𝐿 𝐵𝒾𝑔 𝐵𝓇𝒶𝒾𝓃𝓈  F𝕽𝐸𝒜𝒦  🍪𝐹  𝒯𝐻𝐸 𝒲𝐸𝐸𝒦'!!!! " \
-                        "Enter code `FREAK` at checkout on mypillow.com to claim your reward 🐘  🎀"
-        post_text(post_str)
-#
+    # if dayno == 5:
+    #     best = get_weekly_scores()
+    #     post_str = "🍰 ⋆ 🍉  🎀 Congrats to " + best[
+    #         'person'] + " for winning '𝐿𝐿 𝐵𝒾𝑔 𝐵𝓇𝒶𝒾𝓃𝓈  F𝕽𝐸𝒜𝒦  🍪𝐹  𝒯𝐻𝐸 𝒲𝐸𝐸𝒦'!!!! " \
+    #                     "Enter code `FREAK` at checkout on mypillow.com to claim your reward 🐘  🎀"
+    #     post_text(post_str)
+    #
